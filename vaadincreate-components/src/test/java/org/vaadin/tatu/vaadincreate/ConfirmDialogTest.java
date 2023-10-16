3e3d8ae575fc8e141f9e6d61d46f17318901057c
TatuWorkLenovo\Tatu <tatu@vaadin.com>
2023-10-16 18:14:36 +0300

    Add testing the confirm event

--- vaadincreate-components/src/test/java/org/vaadin/tatu/vaadincreate/ConfirmDialogTest.java
+++ vaadincreate-components/src/test/java/org/vaadin/tatu/vaadincreate/ConfirmDialogTest.java
@@ -1,9 +1,13 @@
 package org.vaadin.tatu.vaadincreate;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.vaadin.tatu.vaadincreate.ConfirmDialog.Type;
 
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
@@ -18,5 +22,20 @@ public class ConfirmDialogTest {
         var content = (VerticalLayout) dialog.getContent();
         var message = (Label) content.getComponent(0);
         Assert.assertEquals("Are you sure?", message.getValue());
+    }
+
+    @Test
+    public void confirmEvent() {
+        var dialog = new ConfirmDialog("Are you sure?", Type.ALERT);
+
+        var content = (VerticalLayout) dialog.getContent();
+        var buttons = (HorizontalLayout) content.getComponent(1);
+        var confirmButton = (Button) buttons.getComponent(1);
+        var count = new AtomicInteger(0);
+        dialog.addConfirmedListener(e -> {
+            count.addAndGet(1);
+        });
+        confirmButton.click();
+        Assert.assertEquals(1, count.get());
     }
 }

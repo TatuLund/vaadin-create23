35e25d25c089434acd814511d3d0feb8b655205e
TatuJLund <tatulund@live.com>
2024-11-30 12:33:26 +0200

    test: Add reset button keyboard operation test case

--- vaadincreate-components/src/test/java/org/vaadin/tatu/vaadincreate/ResetButtonForTextFieldIT.java
+++ vaadincreate-components/src/test/java/org/vaadin/tatu/vaadincreate/ResetButtonForTextFieldIT.java
@@ -3,6 +3,7 @@ package org.vaadin.tatu.vaadincreate;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import com.vaadin.testbench.elements.LabelElement;
 import com.vaadin.testbench.elements.TextFieldElement;
@@ -29,4 +30,23 @@ public class ResetButtonForTextFieldIT extends AbstractComponentTest {
         Assert.assertEquals("Value:", value.getText());
         Assert.assertFalse(button.isDisplayed());
     }
+
+    @Test
+    public void resetButtonFoundAndClearsKeyboard() {
+        var field = $(TextFieldElement.class).first();
+        Assert.assertTrue(field.getClassNames()
+                .contains("resetbuttonfortextfield-textfield"));
+        var button = driver.findElement(
+                By.className("resetbuttonfortextfield-resetbutton"));
+        Assert.assertTrue(button.isDisplayed());
+
+        field.focus();
+        field.sendKeys(Keys.TAB, Keys.ENTER);
+
+        Assert.assertEquals("", field.getValue());
+        var value = $(LabelElement.class).first();
+        Assert.assertEquals("Value:", value.getText());
+        Assert.assertFalse(button.isDisplayed());
+    }
+
 }

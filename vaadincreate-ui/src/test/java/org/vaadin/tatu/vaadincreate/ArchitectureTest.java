bd1c829c23fe8d45b6d930f1f93dcc5fa224012a
TatuJLund <tatulund@live.com>
2025-01-26 13:57:20 +0200

    test: Add more architecture tests and improve naming of the tests

--- vaadincreate-ui/src/test/java/org/vaadin/tatu/vaadincreate/ArchitectureTest.java
+++ vaadincreate-ui/src/test/java/org/vaadin/tatu/vaadincreate/ArchitectureTest.java
@@ -1,11 +1,22 @@
 package org.vaadin.tatu.vaadincreate;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import static com.tngtech.archunit.lang.conditions.ArchPredicates.*;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.*;
 
 import com.tngtech.archunit.lang.ArchRule;
+import com.vaadin.navigator.View;
 
+import org.hibernate.SessionFactory;
 import org.junit.runner.RunWith;
+import org.vaadin.tatu.vaadincreate.auth.AllPermitted;
+import org.vaadin.tatu.vaadincreate.auth.RolesPermitted;
+import org.vaadin.tatu.vaadincreate.backend.dao.HibernateUtil;
+import org.vaadin.tatu.vaadincreate.i18n.I18n;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -30,87 +41,138 @@ import com.tngtech.archunit.junit.ArchUnitRunner;
 public class ArchitectureTest {
 
     @ArchTest
-    public static final ArchRule servicesAreCalledOnlyByPresetersUIandAuth = classes()
+    public static final ArchRule Services_are_called_only_by_the_Preseters_UI_and_Auth = classes()
             .that().resideInAPackage("..backend..").and()
             .haveSimpleNameEndingWith("Service").should().onlyBeAccessed()
-            .byClassesThat(simpleNameEndingWith("Presenter")
-                    .or(simpleNameEndingWith("Test"))
-                    .or(simpleName("VaadinCreateUI"))
-                    .or(simpleName("AboutView"))
+            .byClassesThat(have(simpleNameEndingWith("Presenter"))
+                    .or(have(simpleNameEndingWith("Test")))
+                    .or(have(simpleName("VaadinCreateUI")))
+                    .or(have(simpleName("AboutView")))
                     .or(resideInAPackage("..auth.."))
-                    .or(resideInAPackage("..backend..")));
+                    .or(resideInAPackage("..backend..")))
+            .as("UI implementing code should not depend on backend");
 
     @ArchTest
-    public static final ArchRule eventBusIsOnlyUsedByPresentersAndUI = classes()
+    public static final ArchRule EventBus_is_only_used_by_Presenters_and_UI = classes()
             .that().resideInAPackage("..vaadincreate.eventbus..").should()
             .onlyBeAccessed()
-            .byClassesThat(simpleNameEndingWith("Presenter")
-                    .or(simpleNameEndingWith("Test"))
-                    .or(simpleName("VaadinCreateUI"))
-                    .or(simpleName("AboutView"))
+            .byClassesThat(have(simpleNameEndingWith("Presenter"))
+                    .or(have(simpleNameEndingWith("Test")))
+                    .or(have(simpleName("VaadinCreateUI")))
+                    .or(have(simpleName("AboutView")))
                     .or(resideInAPackage("..eventbus.."))
                     .or(resideInAPackage("..locking.."))
                     .or(resideInAPackage("..auth.."))
                     .or(resideInAPackage("..backend..")));
 
     @ArchTest
-    public static final ArchRule daosAreOnlyUsedByServices = classes().that()
-            .haveSimpleNameEndingWith("Dao").should().onlyBeAccessed()
+    public static final ArchRule only_the_Services_may_use_DAOs = classes()
+            .that().haveSimpleNameEndingWith("Dao").should().onlyBeAccessed()
             .byClassesThat(resideInAPackage("..service..")
-                    .or(resideInAPackage("..dao..")));
+                    .or(resideInAPackage("..dao..")))
+            .as("DAOs should be used only by services");
 
     @ArchTest
-    public static final ArchRule backendShouldNotUseVaadin = classes().that()
-            .resideInAPackage("com.vaadin..").should().onlyAccessClassesThat()
-            .resideOutsideOfPackage("..backend..");
+    // Only the HibernateUtil may use SessionFactory
+    public static final ArchRule only_DAOs_may_use_the_SessionFactory = noClasses()
+            .that().resideOutsideOfPackage("..dao..").should()
+            .accessClassesThat().areAssignableTo(SessionFactory.class)
+            .as("DAOs may use the " + SessionFactory.class.getSimpleName()
+                    + " only via HibernateUtil");
 
     @ArchTest
-    public static final ArchRule serviceImplShouldBeUsedOnlyByBackend = classes()
+    public static final ArchRule only_DAOs_may_use_the_HibernateUtil = noClasses()
+            .that().resideOutsideOfPackage("..dao..").should()
+            .accessClassesThat().areAssignableTo(HibernateUtil.class)
+            .as("Only DAOs may use the " + HibernateUtil.class.getSimpleName());
+
+    @ArchTest
+    public static final ArchRule backend_should_not_use_Vaadin = classes()
+            .that().resideInAPackage("com.vaadin..").should()
+            .onlyAccessClassesThat().resideOutsideOfPackage("..backend..")
+            .as("backend should not depend on Vaadin");
+
+    @ArchTest
+    public static final ArchRule ServiceImpl_should_be_used_only_by_backend = classes()
             .that().resideInAPackage("..backend..").and()
             .haveSimpleNameEndingWith("ServiceImpl").should().onlyBeAccessed()
-            .byClassesThat().resideInAPackage("..backend..");
+            .byClassesThat().resideInAPackage("..backend..")
+            .as("Service implementations should be used only via their interfaces");
 
     @ArchTest
-    public static final ArchRule presentersShouldNotUseVaadin = classes().that()
-            .haveSimpleNameEndingWith("Presenter").should()
-            .onlyAccessClassesThat().resideOutsideOfPackage("com.vaadin..");
+    public static final ArchRule Presenters_should_not_use_Vaadin = classes()
+            .that().haveSimpleNameEndingWith("Presenter").should()
+            .onlyAccessClassesThat().resideOutsideOfPackage("com.vaadin..")
+            .as("Presenters should not have UI implementing code");
 
     @ArchTest
-    public static final ArchRule appLayoutShouldBeUsedOnlyByVaadinCreateUI = classes()
+    public static final ArchRule AppLayout_should_be_used_only_by_VaadinCreateUI = classes()
             .that().haveSimpleName("AppLayout").should().onlyBeAccessed()
-            .byClassesThat(simpleNameEndingWith("Test")
-                    .or(simpleName("VaadinCreateUI")).or(simpleName("AppLayout")
+            .byClassesThat(have(simpleNameEndingWith("Test"))
+                    .or(have(simpleName("VaadinCreateUI")))
+                    .or(have(simpleName("AppLayout"))
                             .or(belongTo(simpleName("AppLayout")))));
 
     @ArchTest
-    public static final ArchRule loginViewShouldBeUsedOnlyByVaadinCreateUI = classes()
+    public static final ArchRule LoginView_should_be_used_only_by_VaadinCreateUI = classes()
             .that().haveSimpleName("LoginView").should().onlyBeAccessed()
-            .byClassesThat(simpleNameEndingWith("Test")
-                    .or(simpleName("VaadinCreateUI"))
-                    .or(simpleName("LoginView")));
+            .byClassesThat(have(simpleNameEndingWith("Test"))
+                    .or(have(simpleName("VaadinCreateUI")))
+                    .or(have(simpleName("LoginView"))));
 
     @ArchTest
-    public static final ArchRule errorViewShouldBeUsedOnlyByAppLayout = classes()
+    public static final ArchRule ErrorView_should_be_used_only_by_AppLayout = classes()
             .that().haveSimpleName("ErrorView").should().onlyBeAccessed()
             .byClassesThat(simpleName("AppLayout").or(simpleName("ErrorView")));
 
     @ArchTest
-    public static final ArchRule basicAccessControlShouldBeUsedOnlyByAuth = classes()
+    public static final ArchRule BasicAccessControl_should_be_used_only_by_auth = classes()
             .that().haveSimpleName("BasicAccessControl").should()
-            .onlyBeAccessed().byClassesThat(
-                    simpleNameEndingWith("Test").or(simpleName("AccessControl"))
-                            .or(simpleName("VaadinCreateUI"))
-                            .or(simpleName("BasicAccessControl")));
+            .onlyBeAccessed()
+            .byClassesThat(simpleNameEndingWith("Test")
+                    .or(have(simpleName("AccessControl")))
+                    .or(have(simpleName("VaadinCreateUI")))
+                    .or(have(simpleName("BasicAccessControl"))));
 
     @ArchTest
-    public static final ArchRule eventBusImplShouldBeUsedOnlyByEventBus = classes()
+    public static final ArchRule EventBusImpl_should_be_used_only_by_EventBus = classes()
             .that().haveSimpleName("EventBusImpl").should().onlyBeAccessed()
-            .byClassesThat(
-                    simpleName("EventBus").or(simpleName("EventBusImpl")));
+            .byClassesThat(have(simpleName("EventBus"))
+                    .or(have(simpleName("EventBusImpl"))))
+            .as("EventBus should be used only view interface");
 
     @ArchTest
-    public static final ArchRule lockedObjectsImplShouldBeUsedOnlyByLockedObjects = classes()
+    public static final ArchRule LockedObjectsImpl_should_be_used_only_by_LockedObjects = classes()
             .that().haveSimpleName("LockedObjectsImpl").should()
-            .onlyBeAccessed().byClassesThat(simpleName("LockedObjects")
-                    .or(simpleName("LockedObjectsImpl")));
+            .onlyBeAccessed()
+            .byClassesThat(have(simpleName("LockedObjects"))
+                    .or(have(simpleName("LockedObjectsImpl"))))
+            .as("LockedObjects should be used only view interface");
+
+    @ArchTest
+    public static final ArchRule views_should_have_security_annotation = classes()
+            .that().resideInAPackage("org.vaadin.tatu.vaadincreate").and()
+            .haveSimpleNameEndingWith("View").and().implement(View.class)
+            .should().beAnnotatedWith(AllPermitted.class).orShould()
+            .beAnnotatedWith(RolesPermitted.class)
+            .as("Views should have access control annotation");
+
+    @ArchTest
+    public static final ArchRule I18ns_fields_should_be_constants = fields()
+            .that().areDeclaredIn(I18n.class).or()
+            .areDeclaredInClassesThat(belongTo(simpleName("I18n"))).should()
+            .beStatic().andShould().beFinal();
+
+    @ArchTest
+    public static final ArchRule VaadinCreateThemes_fields_should_be_constants = fields()
+            .that().areDeclaredIn(VaadinCreateTheme.class).should().beStatic()
+            .andShould().beFinal();
+
+    @ArchTest
+    public static final ArchRule utility_methods_should_be_static = methods()
+            .that()
+            .areDeclaredInClassesThat(
+                    resideInAPackage("org.vaadin.tatu.vaadincreate.util"))
+            .should().beStatic();
+
 }

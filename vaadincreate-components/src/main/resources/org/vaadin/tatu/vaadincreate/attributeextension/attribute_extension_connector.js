b3e3119a204063404abd5145079f26615e54fb21
TatuJLund <tatulund@live.com>
2025-02-18 22:21:48 +0200

    feat: Add JavaScript compress

--- vaadincreate-components/src/main/resources/org/vaadin/tatu/vaadincreate/attributeextension/attribute_extension_connector.js
+++ vaadincreate-components/src/main/resources/org/vaadin/tatu/vaadincreate/attributeextension/attribute_extension_connector.js
@@ -3,16 +3,18 @@ window.org_vaadin_tatu_vaadincreate_AttributeExtension = function () {
     const element = this.getElement(this.getParentId());
     if (element) {
       const attributes = this.getState().attributes;
-      for (let attr in attributes) {
+      for (var attr in attributes) {
         if (attributes.hasOwnProperty(attr)) {
           try {
             element.setAttribute(attr, attributes[attr]);
           } catch (e) {
-            // IE8 does not support type='number' - just ignore it
+            console.error('Failed to set attribute ' + attr + ' to ' + attributes[attr], e);
           }
         }
       }
-      this.getState().removals.forEach(attr => element.removeAttribute(attr));
+      this.getState().removals.forEach(function(attr) {
+        element.removeAttribute(attr);
+      });
     }
   };
 };
